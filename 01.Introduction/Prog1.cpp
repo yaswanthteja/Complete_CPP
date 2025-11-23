4d12d46e222803c9a8f1bbd6a7099641dc567367
yaswanthteja <yaswanthteja02@gmail.com>
2025-11-23 18:51:03 +0530

    added language fundaments

---
+++ 01.Introduction/Prog1.cpp
@@ -0,0 +1,8 @@
+// Prog1.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Yaswanth!" << endl;
+    return 0;
+}
---
+++ 02.LanguageBasics/add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=10;
+    int b=20;
+    int result =a+b;
+    cout<< "The result of a+b is " << result;
+    return 0;
+}
