ef5d3b8b2acbf217ca372ff5805514c3a51ac9a4
yaswanthteja <yaswanthteja02@gmail.com>
2025-12-17 15:30:21 +0530

    added jump statements

---
+++ 04.IterativeStatements/InterviewQna/skipEven.cpp
@@ -0,0 +1,24 @@
+
+//Skip even numbers
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int num;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    for(int i=1;i<num;i++){
+        if(i%2==0){
+            continue;
+        }
+        cout<< i <<endl;
+    }
+
+    return 0;
+}
